fbbe36cbd04692577d3ca028217af95b7f2196a7
Arun <arunchandra738@gmail.com>
2024-10-05 23:01:23 +0530

    Stack LL Menu driven

--- Stack_LL.cpp
+++ Stack_LL.cpp
@@ -1,117 +1,118 @@
 #include <iostream>
-#include "Node.h"
 using namespace std;
 
+// Node class definition
 template <class T>
-class Stack{
+class Node {
+public:
+    T data;
+    Node* next;
 
-    Node<T> *head;
-    Node<T> *tail;
-
-    public:
-
-    Stack(){
-        head = nullptr;
-        tail = nullptr;
-    }
-
-    void push(T x){
-        
-        Node<T> *newNode = new Node<T>(x);
-        if(isEmpty()){
-
-            // cout<<"hello";
-            head = newNode;
-            tail = head;
-            return;
-        }
-        
-        tail->next = newNode;
-        tail = newNode;
-    }
-
-    T pop(){
-        
-        if(isEmpty()){
-            cout<< "Stack is empty!";
-            return T() ;
-        }
-        Node<T> *temp = head;
-        Node<T> *prev = nullptr;
-
-        if( head==tail){
-            T element = head->data;
-            Node<T> *toDelete = head;
-            head = nullptr;
-            tail = nullptr;
-            delete toDelete;
-            return element;
-        }
-
-        while(temp->next!=tail){
-            temp = temp->next; 
-        }
-
-        T element = tail->data;
-        delete tail;
-        
-        tail = temp;
-        tail->next = nullptr;
-        return element;
-    }
-
-    T topEl(){
-        if(isEmpty()){
-            cout<<"Stack is Empty!";
-            return T();
-        }
-
-        return tail->data;
-    }
-
-    bool isEmpty(){
-        if(head==nullptr && tail == nullptr){
-            return true;
-        }
-
-        return false;
-    }
-
-    void printStack(){
-
-        if(isEmpty()){
-            cout<<"Stack is Empty!";
-            return;
-        }
-
-        Node<T> *temp = head;
-
-        while(temp!=nullptr){
-
-            cout<< temp->data << " | ";
-            temp = temp->next;
-
-        }
-        cout<<endl;
-
-    }
-
-
+    Node(T val) : data(val), next(nullptr) {}
 };
 
+// Stack class definition
+template <class T>
+class Stack {
+    Node<T>* head; // Points to the top of the stack
 
+public:
+    Stack() : head(nullptr) {}
 
-int main(){
-    Stack <int> s;
-    s.push(51);
-    s.push(51);
-    s.push(51);
-    s.pop();
-    s.pop();
-    s.push(12);
-    s.push(12);
-    s.push(12);
-    s.push(12);
-    s.printStack();
+    void push(T x) {
+        Node<T>* newNode = new Node<T>(x);
+        newNode->next = head; // Link new node to current head
+        head = newNode;       // Update head to new node
+    }
 
+    T pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty!" << endl;
+            return T(); // Return default value
+        }
+        Node<T>* temp = head; // The current head is the top of the stack
+        T element = head->data; // Get the data from the head
+        head = head->next; // Move head to the next node
+        delete temp; // Free the old head node
+        return element; // Return the popped element
+    }
+
+    T topEl() {
+        if (isEmpty()) {
+            cout << "Stack is Empty!" << endl;
+            return T(); // Return default value
+        }
+        return head->data; // Return the data at head
+    }
+
+    bool isEmpty() {
+        return head == nullptr; // Check if head is null
+    }
+
+    void printStack() {
+        if (isEmpty()) {
+            cout << "Stack is Empty!" << endl;
+            return;
+        }
+        Node<T>* temp = head; // Start from the head
+        while (temp != nullptr) {
+            cout << temp->data << " | ";
+            temp = temp->next; // Move to the next node
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Stack<int> s; // Create a stack of integers
+    int option; // Variable to hold user choice
+
+    do {
+        cout << "\nMenu Driven Program:\n";
+        cout << "1. Push Element\n";
+        cout << "2. Pop Element\n";
+        cout << "3. Check Top Element\n";
+        cout << "4. Check Empty\n";
+        cout << "5. Print Stack Contents\n";
+        cout << "6. Exit the program\n";
+        cout << "Enter your choice: ";
+        cin >> option; // Get user choice
+
+        switch (option) {
+            case 1: {
+                int element;
+                cout << "Enter an element to push: ";
+                cin >> element;
+                s.push(element);
+                cout << "Element " << element << " successfully pushed into the stack.\n";
+                break;
+            }
+            case 2: {
+                int element = s.pop();
+                if (element) { // Check if pop was successful
+                    cout << "Element " << element << " successfully popped from the stack.\n";
+                }
+                break;
+            }
+            case 3:
+                if (!s.isEmpty()) {
+                    cout << "Top element: " << s.topEl() << endl;
+                }
+                break;
+            case 4:
+                cout << (s.isEmpty() ? "Stack is empty!" : "Stack is not empty!") << endl;
+                break;
+            case 5:
+                s.printStack();
+                break;
+            case 6:
+                cout << "Exiting the program.\n";
+                break;
+            default:
+                cout << "Invalid Input. Please enter a valid option!\n";
+                break;
+        }
+    } while (option != 6);
+
+    return 0;
 }
